b738161dc1615dcc82edaf95c0acde8bfad25dea
= <=>
2024-05-03 15:52:23 -0400

    Agregado environments

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,16 +3,26 @@ import { AppController } from './app.controller';
 import { RecintosModule } from './recintos/recintos.module';
 import { ConfigModule } from '@nestjs/config';
 import { MongooseModule } from '@nestjs/mongoose';
+import { EnvConfiguration } from './config/env.config';
 
 @Module({
   imports: [
     ConfigModule.forRoot({
+      // load: [ EnvConfiguration ],         //// Para que las variabel de entorno esten disponibles en toda la app
       isGlobal: true,
     }),
-    MongooseModule.forRoot('mongodb+srv://arpanet80:Dante2011@clusterdespliegue.jmodcqi.mongodb.net/despliegueted'),
+    // MongooseModule.forRoot('mongodb+srv://arpanet80:Dante2011@clusterdespliegue.jmodcqi.mongodb.net/despliegueted'),
+    MongooseModule.forRoot( process.env.MONGODB),
     RecintosModule
   ],
   controllers: [AppController],
   providers: [],
 })
-export class AppModule {}
+export class AppModule {
+
+  
+  //// eso olo para ver que las variables de entorno se cargan
+  // constructor() {
+  //   console.log(process.env);
+  // }
+}
