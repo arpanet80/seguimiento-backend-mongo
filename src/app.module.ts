6a466700dc8f6dd9d40b63e0723ed97b7f4295d1
= <=>
2024-05-07 15:49:58 -0400

    Agregado variables de entonro

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,12 +3,10 @@ import { AppController } from './app.controller';
 import { RecintosModule } from './recintos/recintos.module';
 import { ConfigModule } from '@nestjs/config';
 import { MongooseModule } from '@nestjs/mongoose';
-import { EnvConfiguration } from './config/env.config';
 
 @Module({
   imports: [
     ConfigModule.forRoot({
-      // load: [ EnvConfiguration ],         //// Para que las variabel de entorno esten disponibles en toda la app
       isGlobal: true,
     }),
     // MongooseModule.forRoot('mongodb+srv://arpanet80:Dante2011@clusterdespliegue.jmodcqi.mongodb.net/despliegueted'),
